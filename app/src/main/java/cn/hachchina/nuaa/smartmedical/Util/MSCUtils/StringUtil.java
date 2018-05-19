2eec520c17130d5c6cc441568994822e714f80c1
= <=>
2018-05-19 18:08:29 +0800

    新增语音识别、语音合成功能
    需在任意activity中使用，继承VoiceHelper
    
    实现两个回调

---
+++ app/src/main/java/cn/hachchina/nuaa/smartmedical/Util/MSCUtils/StringUtil.java
@@ -0,0 +1,11 @@
+package cn.hachchina.nuaa.smartmedical.Util.MSCUtils;
+
+/**
+ * Created by VULCAN on 2018/5/19.
+ */
+
+public class StringUtil {
+    public static String getRandomNameString(){
+        return "voice"+System.currentTimeMillis();
+    }
+}
